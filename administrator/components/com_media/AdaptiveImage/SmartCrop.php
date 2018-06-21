d6783ac30c6e6ed6cc5e9af3255f51797219eec2
Geetanshu Mathur <mathur.geetanshu887@gmail.com>
2018-06-22 02:53:56 +0530

    setting base for SmartCrop Class

---
+++ administrator/components/com_media/AdaptiveImage/SmartCrop.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_media
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Media\Administrator\AdaptiveImage;
+
+defined('_JEXEC') or die;
+
+/**
+ * Used for cropping of the images around
+ * the focus points.
+ *
+ * @since  4.0.0
+ */
+class SmartCrop
+{
+    public static $imgSrc;
+    
+    public function __constructor()
+    {
+
+    }
+    public function getFocus()
+    {
+
+    }
+    public function cropImage()
+    {
+
+    }
+    public function getOptimumDimentions()
+    {
+
+    }
+    public function saveImage()
+    {
+
+    }
+}
